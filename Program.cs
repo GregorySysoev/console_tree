9feb3b3898ce0a4d96ecb0fc3a1bb8c0a7c1fbdd
gregorysysoev <gregorysysoev@gmail.com>
2019-07-24 16:09:55 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = System.Environment.CurrentDirectory;
+            Console.WriteLine(path);
+        }
+    }
+}
